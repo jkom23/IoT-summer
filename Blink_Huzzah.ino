50d9656c269271318a78841e69ce35a988884684
Jack Komaroff <jackkomaroff23@gmail.com>
2020-07-15 13:50:51 -0400

    Create Blink_Huzzah.ino

---
+++ Blink_Huzzah.ino
@@ -0,0 +1,36 @@
+/*
+  ESP8266 Blink by Simon Peter
+  Blink the blue LED on the ESP-01 module
+  This example code is in the public domain
+
+  The blue LED on the ESP-01 module is connected to GPIO1
+  (which is also the TXD pin; so we cannot use Serial.print() at the same time)
+
+  Note that this sketch uses LED_BUILTIN to find the pin with the internal LED
+*/
+
+int LED_PIN = 16;
+
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);     // Initialize the LED_BUILTIN pin as an output
+  pinMode(16, OUTPUT);
+  
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  digitalWrite(LED_PIN, LOW);   // Turn the LED on (Note that LOW is the voltage level
+  // but actually the LED is on; this is because
+  // it is active low on the ESP-01)
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(1000);                      // Wait for a second
+  digitalWrite(LED_PIN, HIGH);  // Turn the LED off by making the voltage HIGH
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(2000);                      // Wait for two seconds (to demonstrate the active low LED)
+  digitalWrite(LED_PIN, LOW);   // Turn the LED on (Note that LOW is the voltage level
+  // but actually the LED is on; this is because
+  // it is active low on the ESP-01)
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(1000);                      // Wait for a second
+
+}
