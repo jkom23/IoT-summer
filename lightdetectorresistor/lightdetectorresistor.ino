b9e26c3c4788b046ab6aa049b66276ac33680f8d
Jack Komaroff <jackkomaroff23@gmail.com>
2020-07-15 14:55:15 -0400

    ldr code

---
+++ lightdetectorresistor/lightdetectorresistor.ino
@@ -0,0 +1,16 @@
+int ldr_pin = A0;
+int analogValue=0;
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+analogValue = analogRead(ldr_pin);
+Serial.println(analogValue);
+delay(50);
+
+
+
+}
