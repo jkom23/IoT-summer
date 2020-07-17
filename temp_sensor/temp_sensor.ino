fb129075928134197da359827aee5c7cb99e2b2c
Jack Komaroff <jackkomaroff23@gmail.com>
2020-07-17 12:18:04 -0400

    Create temp_sensor.ino

---
+++ temp_sensor/temp_sensor.ino
@@ -0,0 +1,28 @@
+#include <DHT.h>
+
+#define DHTPIN 4
+#define DHTTYPE DHT22
+
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  
+  Serial.println("DHT Test");
+  dht.begin();
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  float t= dht.readTemperature(true);
+  float h = dht.readHumidity();
+  float c = dht.convertFtoC(t);
+  
+  Serial.printf("\nThe Temperature in the room is %f", t);
+  Serial.printf("\nThe Humidity in the room is %f", h);
+  
+  delay(2000);
+}
