6dc42198b1dd5e14e5dbdd19b521acad8c5e97c0
Jack Komaroff <jackkomaroff23@gmail.com>
2020-07-20 15:53:54 -0400

    Update graphicstest_featherwingID.ino

--- graphicstest_featherwingID/graphicstest_featherwingID.ino
+++ graphicstest_featherwingID/graphicstest_featherwingID.ino
@@ -94,16 +94,13 @@ void setup() {
 
   jackID();
 
-
-
 }
 
+void loop() {
 
-void loop(){
-  
 }
 void jackID() {
-  
+
   tft.fillScreen(ILI9341_BLACK);
   yield();
   tft.fillScreen(ILI9341_WHITE);
@@ -114,19 +111,15 @@ void jackID() {
   tft.setCursor(3, 35);
   tft.setTextColor(ILI9341_RED);  tft.setTextSize(1);
   tft.println("231 West 246th Street; Bronx, NY, 10471");
-  tft.setCursor(40,45);
-   tft.setTextColor(ILI9341_BLACK);  tft.setTextSize(2);
+  tft.setCursor(40, 45);
+  tft.setTextColor(ILI9341_BLACK);  tft.setTextSize(2);
   tft.println("Jack Komaroff");
-  tft.setCursor(40,65);
+  tft.setCursor(40, 65);
   tft.println("Class of: 2023");
-  tft.setCursor(30,85);
+  tft.setCursor(30, 85);
   tft.setTextColor(ILI9341_BLACK);  tft.setTextSize(2);
   tft.println("*4HMLL00398421*");
-  tft.drawRGBBitmap(0,100,self, image_width, image_height);
+  tft.drawRGBBitmap(0, 100, self, image_width, image_height);
   yield();
- 
+
 }
-
-
-
- 
